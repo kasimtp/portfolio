11c843e14e74d1e319e6c2ad8a6535e92853a3eb
tpkasimvlr <tpkasimvlr@gmail.com>
2025-06-14 18:19:11 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Hero from './components/Hero'
 import ParticleBackground from './components/Particle'
 import About from './components/About'
 import Projects from './components/Projects'
-// import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
