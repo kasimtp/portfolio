0177ca3a10117900740a71d4f7d3c5a5021a7f24
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2025-06-16 22:17:02 +0530

    fix: remove tailwindcss vite plugin and use postcss config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// TailwindCSS plugin വേണ്ട, കാരണം PostCSS വഴി connect ചെയ്യും.
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),],
 })
